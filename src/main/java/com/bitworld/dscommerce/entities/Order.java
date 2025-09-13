18b4e4b2daec0ba6e7be5cc5b7daca35f37fa1d7
jppokedev <jppokedev@gmail.com>
2025-09-13 12:26:19 -0300

    Criando a classe Payment

--- src/main/java/com/bitworld/dscommerce/entities/Order.java
+++ src/main/java/com/bitworld/dscommerce/entities/Order.java
@@ -16,8 +16,62 @@ public class Order {
     private Instant moment;
     private OrderStatus status;
 
-    @ManyToOne // faça a relação/mapeamento, muitos para um
+    @ManyToOne // faz a relação/mapeamento, muitos para um
     @JoinColumn(name = "client_id")
     private User client;
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL) // cascade = CascadeType.ALL diz que qualquer operação feita nessa entidade,
+    // também será feita automaticamente na entidade relacionada.
+    private Payment payment;
+
+    public Order(){
+    }
+
+    public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+        this.payment = payment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
 }
