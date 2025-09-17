d022be76b8ed6315a79e9e7fb9917fac41091d8d
jppokedev <jppokedev@gmail.com>
2025-09-17 11:33:22 -0300

    Criado a inserção de novos produtos

--- src/main/java/com/bitworld/dscommerce/controllers/ProductController.java
+++ src/main/java/com/bitworld/dscommerce/controllers/ProductController.java
@@ -5,11 +5,11 @@ import com.bitworld.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController// informa pro spring que esse componete e um controlador rest
@@ -21,12 +21,21 @@ public class ProductController {
 
     @GetMapping(value = "/{id}")
     // informa qual o metodo rest vai ser usado e faz o mapeamento ou seja quando eu chamar o /products ele vai vir aqui buscar esse metodo
-    public ProductDTO findById(@PathVariable Long id) {// a @pathVariable pega o valor passado na url e passa como parametro para o metodo
-        return service.findById(id);// ta pegando o dto findbyid e retornando ele pra requisição
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {// a @pathVariable pega o valor passado na url e passa como parametro para o metodo
+        ProductDTO dto = service.findById(id);// ta pegando o dto findbyid e retornando ele pra requisição
+        return ResponseEntity.ok(dto); // com o responseEntity eu customizo a resposta que vai ser uma resposta 200 por conta do .ok, onde o corpo vai ser o que esta no ()
     }
 
     @GetMapping
-    public Page<ProductDTO> findAll(Pageable pageable){ // o Pageable faz a paginação do findAll, tem que ser o import do springframework.data.domain.Pageable;
-        return service.findAll(pageable);
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable){ // o Pageable faz a paginação do findAll, tem que ser o import do springframework.data.domain.Pageable;
+        Page<ProductDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto){ //ResquestBody faz com que o corpo da requisição enviada no formato json entre no parametro e vire um productDto
+        dto = service.insert(dto); // nao preciso declarar o dto pq estou usando a mesma variavel que veio como parametro
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);// devo fazer isso pq é uma boa pratica para retorna o codigo correto na requisição e ter o link do recurso criado que é a uri criada a cima
     }
 }
